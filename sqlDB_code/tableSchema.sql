a3b4b2185e71064e8189a41bd66b2264e1345c1a
Ding5454 <domingorodriguez2012@gmail.com>
2019-12-12 20:37:06 -0600

    initial table setup and commit

---
+++ sqlDB_code/tableSchema.sql
@@ -0,0 +1,29 @@
+DROP TABLE IF EXISTS teams;
+
+CREATE TABLE teams (
+	team_id VARCHAR(4) NOT NULL PRIMARY KEY,
+	team_name VARCHAR,
+    y1999 INT,
+    y2000 INT,
+    y2001 INT,
+    y2002 INT,
+    y2003 INT,
+    y2004 INT,
+    y2005 INT,
+    y2006 INT,
+    y2007 INT,
+    y2008 INT,
+    y2009 INT,
+    y2010 INT,
+    y2011 INT,
+    y2012 INT,
+    y2013 INT,
+    y2014 INT, 
+    y2015 INT,
+    y2016 INT,
+    y2017 INT,
+    y2018 INT,
+    y2019 INT,
+);
+
+SELECT * FROM teams;

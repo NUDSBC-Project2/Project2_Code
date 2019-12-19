64cf87963dcbc61c39ceb866bd78bc4d5b938ca0
Ding5454 <domingorodriguez2012@gmail.com>
2019-12-18 20:59:16 -0600

    Initial flask framework

---
+++ flaskCode/flaskApp.py
@@ -0,0 +1,17 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route('/heatmap')
+
+
+@app.route('/plot')
+
+
+@app.route('/data')
+
+
+@app.route('/main')
